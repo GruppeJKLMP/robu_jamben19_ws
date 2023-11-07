dcc6812a79b7e839f7d765198b05feda7377eab5
GruppeJKLMP <jamben19@htl-kaindorf.at>
2023-11-07 13:09:21 +0100

    4.Commit

--- src/robu/robu/ex10_wallfollower.py
+++ src/robu/robu/ex10_wallfollower.py
@@ -1,6 +1,6 @@
 import math
 import rclpy
-
+from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
@@ -13,13 +13,13 @@ from enum import IntEnum
 
 
 ROBOT_DRIECTION_FRONT_INDEX = 0
-ROBOT_DIRECTION_RIGHT_FRONT_INDEX = 45
-ROBOT_DIRECTION_RIGHT_INDEX = 90
-ROBOT_DIRECTION_RIGHT_REAR_INDEX = 135
+ROBOT_DIRECTION_RIGHT_FRONT_INDEX = 315
+ROBOT_DIRECTION_RIGHT_INDEX = 270
+ROBOT_DIRECTION_RIGHT_REAR_INDEX = 225
 ROBOT_DIRECTION_REAR_INDEX = 180
-ROBOT_DIRECTION_LEFT_REAR_INDEX = 225
-ROBOT_DIRECTION_LEFT_INDEX = 270
-ROBOT_DIRECTION_LEFT_FRONT_INDEX = 315
+ROBOT_DIRECTION_LEFT_REAR_INDEX = 135
+ROBOT_DIRECTION_LEFT_INDEX =90
+ROBOT_DIRECTION_LEFT_FRONT_INDEX = 45
 
 class WallfollowerStates(IntEnum):
     WF_STATE_DETECTWALL = 0,
@@ -28,7 +28,7 @@ class WallfollowerStates(IntEnum):
     WF_STATE_FOLLOWWALL = 3
 
 
-class WallFollower(rclpy.node.Node):
+class WallFollower(Node):
     def __init__(self):
         super().__init__('WallFollower')
         self.scan_subscriber = self.create_subscription(LaserScan,"/scan",
@@ -44,23 +44,51 @@ class WallFollower(rclpy.node.Node):
         self.rightfront_dist = 9999999.9
         self.right_dist = 9999999.9
         self.rear_dist = 9999999.9
-
+        self.distances = []
         self.wallfollower_state = WallfollowerStates.WF_STATE_DETECTWALL
 
         self.forward_speed_wf_slow = 0.05
         self.forward_speed_wf_fast = 0.1
 
         self.turning_speed_wf_slow = 0.1
-        self.turning_speed_fast = 1.0
-
+        self.turning_speed_wf_fast = 1.0
+        
         self.dist_thresh_wf = 0.3
         self.dist_hysteresis = 0.02
 
+        self.dist_laser_offset = 0.03
+        self.valid_lidar_data = False
         self.timer = self.create_timer(0.2, self.timer_callback)
 
     def timer_callback(self, msg):
-        pass
-    
+        if self.valid_lidar_data: #beim erstmaligen Aufruf ligen noch keine Daten vor
+            self.follow_wall()
+
+    def follow_wall(self):
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
+
+        if self.wallfollower_state == WallfollowerStates.WF_STATE_DETECTWALL:
+            dist_min = min(self.distances)
+            #Drehe Roboter solange bis aud der X-Achse der Mindestabstand erscheint
+            if self.front_dist > (dist_min + self.dist_laser_offset): 
+                # Wenn der Roboter kurz vor deM Mindestabstand ist, verringert dieser die Drehgeschwindigkeit
+                if abs(self.front_dist - dist_min) < 0.2:
+                    msg.angular.z = self.turning_speed_wf_slow
+                else:
+                    msg.angular.z = self.turning_speed_wf_fast
+            else:
+                print("WF_STATE_DRIVE2WALL")
+                self.wallfollower_state = WallfollowerStates.WF_STATE_DRIVE2WALL
+        
+        self.cmd_vel_publisher.publish(msg)
+        
     def scan_callback(self, msg):
         self.left_dist = msg.ranges[ROBOT_DIRECTION_LEFT_INDEX]
         self.leftfront_dist = msg.ranges[ROBOT_DIRECTION_LEFT_FRONT_INDEX]
@@ -68,13 +96,15 @@ class WallFollower(rclpy.node.Node):
         self.rightfront_dist = msg.ranges[ROBOT_DIRECTION_RIGHT_FRONT_INDEX]
         self.right_dist = msg.ranges[ROBOT_DIRECTION_RIGHT_INDEX]
         self.rear_dist = msg.ranges[ROBOT_DIRECTION_REAR_INDEX]
+        self.distances = msg.ranges
 
-    print("ld: %.2f m" % self.left_dist, 
-          "lfd: %.2f m" % self.leftfront_dist,
-          "fd: %.2f m" % self.front_dist,
-          "rfd: %.2f m" % self.rightfront_dist,
-          "rd: %.2f m", % self.rear_dist,
-          "rrd: %.2f m", % self.rear_dist)
+        self.valid_lidar_data = True
+        print("ld: %.2f m" % self.left_dist, 
+            "lfd: %.2f m" % self.leftfront_dist,
+            "fd: %.2f m" % self.front_dist,
+            "rfd: %.2f m" % self.rightfront_dist,
+            "rd: %.2f m" %  self.right_dist,
+            "rrd: %.2f m" % self.rear_dist)
 
 def main(args=None):
     rclpy.init(args=args)
