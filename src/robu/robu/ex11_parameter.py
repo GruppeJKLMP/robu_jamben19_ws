6425a9e0e6bd5b1cf640d36e90b1339ec4c97e71
GruppeJKLMP <jamben19@htl-kaindorf.at>
2024-02-06 12:53:15 +0100

    xter commit

--- src/robu/robu/ex11_parameter.py
+++ src/robu/robu/ex11_parameter.py
@@ -5,12 +5,31 @@ class MinimalParameter(Node):
     def __init__(self):
         super().__init__('MinimalParameter')
 
-        self.declare_parameter('my_parameter')
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('forward_speed_wf_fast', 0.1),
+                ('forward_speed_wf_slow', 0.05),
+                ('turning_speed_wf_fast', 1.0),
+                ('turning_speed_wf_slow', 0.1),
+                ('dist_thresh_wf', 0.3),
+                ('dist_hysteresis_wf', 0.02),
+            ])
 
-        my_param = self.get_parameter('my_parameter')
+        self.timer = self.create_timer(1.0, self.timer_callback)
+        #my_param = self.get_parameter('my_parameter')
+        #print("my parameter:", my_param)
+    def timer_callback(self):
+        forward_speed_wf_fast = self.get_parameter('forward_speed_wf_fast').value
+        forward_speed_wf_slow = self.get_parameter('forward_speed_wf_slow').value
+        turning_speed_wf_fast = self.get_parameter('turning_speed_wf_fast').value
+        turning_speed_wf_slow = self.get_parameter('turning_speed_wf_slow').value
+        dist_thresh_wf = self.get_parameter('dist_thresh_wf').value
+        dist_hysteresis_wf = self.get_parameter('dist_hysteresis_wf').value
 
-        print("my parameter:", my_param)
-
+        self.get_logger().info('forward_speed_wf_fast: %5.2f forward_speed_wf_slow: %5.2f' %(forward_speed_wf_fast, forward_speed_wf_slow) )
+        self.get_logger().info('turning_speed_wf_fast: %5.2f turning_speed_wf_slow: %5.2f' %(turning_speed_wf_fast, turning_speed_wf_slow))
+        self.get_logger().info('dist_thresh_wf:        %5.2f dist_hysteresis_wf:    %5.2f' %(dist_thresh_wf, dist_hysteresis_wf))
 
 def main():
     rclpy.init()
@@ -18,3 +37,6 @@ def main():
     node = MinimalParameter()
 
     rclpy.spin(node)
+
+
+    
